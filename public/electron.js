d6a905b86c906327260fcefec9f3389b8e3578c0
Jevgenijs Pankovs <eugene.pankov@gmail.com>
2023-01-02 18:21:40 +0200

    added electron-forge

--- public/electron.js
+++ public/electron.js
@@ -3,7 +3,13 @@ const path = require("path");
 const { app, BrowserWindow } = require("electron");
 const isDev = require("electron-is-dev");
 
-function createWindow() {
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require("electron-squirrel-startup")) {
+    app.quit();
+}
+
+const createWindow = async () => {
+
     // Create the browser window.
     const win = new BrowserWindow({
         width: 800,
@@ -30,7 +36,11 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+app.whenReady()
+    .then(() => {
+        createWindow();
+    })
+    .catch(console.log);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
